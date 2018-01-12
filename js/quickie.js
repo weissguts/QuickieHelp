35053428b6e6c627a59018050f12cc24bbefd443
Daniel Hernandez <weissguts@gmail.com>
2018-01-11 22:02:45 -0700

    manual merging with becky-branch.

--- js/quickie.js
+++ js/quickie.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// // Next/previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
 function showSlides(n) {
     var i;
@@ -53,9 +53,7 @@ function showSlides(n) {
         };
         var marker = new google.maps.Marker(optionsMarker);
 
-
     }
-
 
     initMap();
 
