15ebfa5635a89d5c81b4cd432c0970850f50b4a9
Daniel Hernandez <weissguts@gmail.com>
2018-01-12 16:28:24 -0700

    Maps now fulling working on all Charity Modals. Jquery event listener was the trick! Need to now update: -cooridinates for each site -Paragraph text descriping event instead of Lorem Ipsum -Event Headers

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
 
-    function initMap() {
+    /*****************Google Map init functions for six base templates. Currently pre-loaded**********************/
+    //Calls google map based on latitude / longitude.
+    function initMapOne() {
         var latlng = new google.maps.LatLng(39.679504, -104.902479);
         //Map options
         var options = {
-            zoom:12,
+            zoom:15,
             center: latlng
         };
 
         //New Map
         var map = new
-        google.maps.Map(document.getElementById('map'), options);
+        google.maps.Map(document.getElementById('mapOne'), options);
 
         //Add Marker
         var optionsMarker = {
@@ -20,7 +22,154 @@ $(document).ready(function () {
         var marker = new google.maps.Marker(optionsMarker);
         google.maps.event.trigger(map, 'resize');
 
-    }
+    };
 
-    initMap();
+    function initMapTwo() {
+        var latlng = new google.maps.LatLng(39.679504, -104.902479);
+        //Map options
+        var options = {
+            zoom:15,
+            center: latlng
+        };
+
+        //New Map
+        var map = new
+        google.maps.Map(document.getElementById('mapTwo'), options);
+
+        //Add Marker
+        var optionsMarker = {
+            position: latlng,
+            map:map
+        };
+        var marker = new google.maps.Marker(optionsMarker);
+        google.maps.event.trigger(map, 'resize');
+
+    };
+
+    function initMapThree() {
+        var latlng = new google.maps.LatLng(39.679504, -104.902479);
+        //Map options
+        var options = {
+            zoom:15,
+            center: latlng
+        };
+
+        //New Map
+        var map = new
+        google.maps.Map(document.getElementById('mapThree'), options);
+
+        //Add Marker
+        var optionsMarker = {
+            position: latlng,
+            map:map
+        };
+        var marker = new google.maps.Marker(optionsMarker);
+        google.maps.event.trigger(map, 'resize');
+
+    };
+
+    function initMapFour() {
+        var latlng = new google.maps.LatLng(39.679504, -104.902479);
+        //Map options
+        var options = {
+            zoom:15,
+            center: latlng
+        };
+
+        //New Map
+        var map = new
+        google.maps.Map(document.getElementById('mapFour'), options);
+
+        //Add Marker
+        var optionsMarker = {
+            position: latlng,
+            map:map
+        };
+        var marker = new google.maps.Marker(optionsMarker);
+        google.maps.event.trigger(map, 'resize');
+
+    };
+
+    function initMapFive() {
+        var latlng = new google.maps.LatLng(39.679504, -104.902479);
+        //Map options
+        var options = {
+            zoom:15,
+            center: latlng
+        };
+
+        //New Map
+        var map = new
+        google.maps.Map(document.getElementById('mapFive'), options);
+
+        //Add Marker
+        var optionsMarker = {
+            position: latlng,
+            map:map
+        };
+        var marker = new google.maps.Marker(optionsMarker);
+        google.maps.event.trigger(map, 'resize');
+
+    };
+
+    function initMapSix() {
+        var latlng = new google.maps.LatLng(39.679504, -104.902479);
+        //Map options
+        var options = {
+            zoom:15,
+            center: latlng
+        };
+
+        //New Map
+        var map = new
+        google.maps.Map(document.getElementById('mapSix'), options);
+
+        //Add Marker
+        var optionsMarker = {
+            position: latlng,
+            map:map
+        };
+        var marker = new google.maps.Marker(optionsMarker);
+        google.maps.event.trigger(map, 'resize');
+
+    };
+
+    /************************Charity Modal google map calls**************************/
+
+    //Charity modal 1
+    $('#mapPuppy').click(function () {
+        initMapOne();
+    });
+
+    //Charity modal 2
+    $('#mapBfast').click(function () {
+        initMapTwo();
+    });
+
+    //Charity modal 3
+    $('#mapEstes').click(function () {
+        initMapThree();
+    });
+
+    //Charity modal 4
+    $('#mapYouth').click(function () {
+        initMapFour();
+    });
+
+    //Charity modal 5
+    $('#mapConstruction').click(function () {
+        initMapFive();
+    });
+
+    //Charity modal 6
+    $('#mapTeach').click(function () {
+        initMapSix();
+    });
+
+
+
+
+
+
+
 });
