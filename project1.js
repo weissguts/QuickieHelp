a608081b0e1d464d31eb2061557e5a9327d8be6f
Marcus <therealthug09@aol.com>
2018-01-12 21:58:56 -0700

    marcus-firebase-changes

--- project1.js
+++ project1.js
@@ -1,206 +1,201 @@
 for (let i = 0; i < 100; i++) {
-    $('#age').append(`
+  $('#age').append(`
     <option value="${[i]}">${[i]}</option>
 `)
 };
 
-// Initialize Firebase
-var config = {
+ // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyCSVExCfvP9TwDQacXgj1lwu1E1uXcq7KI",
     authDomain: "quickiehelp-b8fb4.firebaseapp.com",
     databaseURL: "https://quickiehelp-b8fb4.firebaseio.com",
     projectId: "quickiehelp-b8fb4",
     storageBucket: "quickiehelp-b8fb4.appspot.com",
     messagingSenderId: "1008999421520"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
 
 var database = firebase.database();
 
-var firstName = "";
-var lastName = "";
-var email = "";
-var password = "";
-var age = 0;
-var gender = "";
+    var firstName = "";
+    var lastName = "";
+    var email = "";
+    var password = "";
+    var age = 0;
+    var gender = "";
 
+    
+    
+    // Capture Button Click
+    $("#add-user").on("click", function() {
 
+      event.preventDefault();
+      
+      firstName = $("#firstName").val().trim();
+      lastName = $("#lastName").val().trim();
+      email = $("#email").val().trim();
+      password = $("#password").val().trim();
+      age = $("#age").val().trim();
+      gender = $("#gender").val().trim();
 
-// Capture Button Click
-$("#add-user").on("click", function() {
-
-    event.preventDefault();
-
-    firstName = $("#firstName").val().trim();
-    lastName = $("#lastName").val().trim();
-    email = $("#email").val().trim();
-    password = $("#password").val().trim();
-    age = $("#age").val().trim();
-    gender = $("#gender").val().trim();
-
-    createAcount();
-
-    database.ref().push().set({
-        firstName: firstName,
-        lastName: lastName,
+        createAcount();
+      
+      database.ref().push().set({
+       firstName: firstName,
+       lastName: lastName,
         email: email,
         password: password,
         age: age,
         gender: gender
 
-
+        
+      });
+      // $("#signupForm").reset();
     });
-    // $("#signupForm").reset();
-});
 
-database.ref().on("value", function(snapshot) {
+ database.ref().on("value", function(snapshot) {
+     
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
 
-}, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-});
+  ///////////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////////////////
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-
-
-        document.getElementById("userDiv").style.display = "block";
-        document.getElementById("loginDiv").style.display = "none";
-        // document.getElementById("signupbtn").style.display = "none";
-        document.getElementById("signupNav").style.display = "none";
-        // document.getElementById("loginNav").text = emailId;
+    document.getElementById("userDiv").style.display = "block";
+    document.getElementById("loginDiv").style.display = "none";
+    document.getElementById("signupbtn").style.display = "none";
+    document.getElementById("signupNav").style.display = "none";
+    // document.getElementById("loginNav").text = emailId;
 
 
 
-        var user = firebase.auth().currentUser;
+    var user = firebase.auth().currentUser;
 
-        if (user != null) {
+    if (user != null) {
 
-            var emailId = user.email;
-            var emailVerified = user.emailVerified;
-            if (emailVerified) {
-                document.getElementById("verification").style.display = "none";
-            }
-            else {
+      var emailId = user.email;
+      var emailVerified = user.emailVerified;
+      if (emailVerified) {
+        document.getElementById("verification").style.display = "none";
+      }
+      else {
                 document.getElementById("verification").style.display = "block";
 
-            }
-            document.getElementById("userPara").innerHTML = "Welcome User! :" + emailId + "<br/>Verified: " + emailVerified;
-            // $("#notiSign").append("<h4>Welcome : " + emailId);
-            document.getElementById("loginNav").text = emailId;
+      }
+      document.getElementById("userPara").innerHTML = "Welcome User! :" + emailId + "<br/>Verified: " + emailVerified;
+      // $("#notiSign").append("<h4>Welcome : " + emailId);
+      document.getElementById("loginNav").text = emailId;
 
-
-        }
-    } else {
-        // No user is signed in.
-
-        document.getElementById("userDiv").style.display = "none";
-        document.getElementById("loginDiv").style.display = "block";
-        document.getElementById("loginNav").text = "log in";
-        document.getElementById("signupNav").style.display = "block";
-
-        // $("#notiSign").hide("<h4>Welcome : " + emailId);
+      
     }
+  } else {
+    // No user is signed in.
+
+     document.getElementById("userDiv").style.display = "none";
+    document.getElementById("loginDiv").style.display = "block";
+    document.getElementById("loginNav").text = "log in";
+    document.getElementById("signupNav").style.display = "block";
+
+    // $("#notiSign").hide("<h4>Welcome : " + emailId);
+  }
 });
 
-function createAcount() {
-    // $("#emailField").val();
+  function createAcount() {
+          // $("#emailField").val();
     var userEmail = $("#emailField").val();
     var userPassword = $("#passwordField").val();
 
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-
-        // ...
-    });
+  // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  // ...
+});
 
 
 
-}
+  }
 
-function login() {
+  function login() {
 
     var userEmail = $("#emailField").val();
     var userPassword = $("#passwordField").val();
 
     firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
 
 
 
 
-}
+  }
 
-function sendVerification() {
+  function sendVerification() {
 
     var user = firebase.auth().currentUser;
 
     user.sendEmailVerification().then(function() {
-        // Email sent.
-    }).catch(function(error) {
-        // An error happened.
-    });
+  // Email sent.
+}).catch(function(error) {
+  // An error happened.
+});
 
-}
+  }
 
-function logOut() {
+  function logOut() {
     var user = firebase.auth().signOut();
-}
+  }
 
-// On Clicks
+  // On Clicks
 
-$("#logOut").on("click", function() {
+  $("#logOut").on("click", function() {
     event.preventDefault();
     logOut();
-})
+  })
 
-$("#createAcount").on("click", function() {
+  $("#createAcount").on("click", function() {
     event.preventDefault();
 
     createAcount();
-})
+  })
 
-$("#loginAcount").on("click", function() {
+  $("#loginAcount").on("click", function() {
     event.preventDefault();
 
     login();
-})
+  })
 
-$("#verification").on("click", function() {
+  $("#verification").on("click", function() {
     event.preventDefault();
 
     sendVerification();
-})
-/////////////////////////////////////////////////////////////////////////////
+  })
+  /////////////////////////////////////////////////////////////////////////////
 
-function createAcount() {
-    // $("#emailField").val();
-    var userEmail = $("#emailField").val();
-    console.log(userEmail);
-    var userPassword = $("#passwordField").val();
-    console.log(userPassword);
+  function createAcount() {
+          // $("#emailField").val();
+    var userEmail = $("#email").val();
+    var userPassword = $("#password").val();
 
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
+  // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  // ...
+});
 
 
 
-}
+  }
 
 //////////////////////////////////////////////////////////////////////////////
 // $("#signupForm")[0].reset();
