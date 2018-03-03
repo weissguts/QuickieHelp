ad9ae9fc6433a6e10fffc3382908f3e0a28d31e9
Daniel Hernandez <weissguts@gmail.com>
2018-03-03 08:16:14 -0700

    updated api routes

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -19,8 +19,10 @@ module.exports = function(app) {
     app.post("/api/new", function(req, res) {
         console.log("User Data:");
         console.log(req.body);
-        var dbQuery = "INSERT INTO users (firstname, lastname, email, password, created_at) VALUES (?,?,?,?,?)";
-        connection.query(dbQuery, [req.body.firstname, req.body.lastname, req.body.email, req.body.password, req.body.created_at],
+        var dbQuery = "INSERT INTO users (firstname, lastname, email, password, age, interest, categories, created_at) " +
+            "VALUES (?,?,?,?,?,?,?,?)";
+        connection.query(dbQuery, [req.body.firstname, req.body.lastname, req.body.email, req.body.password, req.body.age,
+                req.body.interest, req.body.categories, req.body.created_at],
             function(err, result) {
             if (err) throw err;
             console.log("User Successfully Saved!");
