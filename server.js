872d6c16206507b27b007f877db81aedf5ccf978
Daniel Hernandez <weissguts@gmail.com>
2018-03-04 14:24:43 -0700

    updated all files, moved all old files in Old_Code folder. Sequelize running, need to generate post.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
-var db = require("./app/models");
+var db = require("./models");
 // Sets up the Express app to handle data parsing
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,8 +21,8 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 // Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
